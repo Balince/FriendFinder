45914bd3504ea9110d4ced6542d8c10c82942d6b
Balince <48064638+Balince@users.noreply.github.com>
2019-10-09 18:39:11 -0400

    Initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+    {
+      name: "John",
+      photo: "https://images.unsplash.com/photo-1535644258845-185656906071?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+        name: "Pink Guy",
+        photo: "https://lyricstranslate.com/files/styles/large/public/Screenshot%202018-01-17%20at%2003.26.34.png?itok=Ya3fM29J",
+        scores: [
+          "3",
+          "1",
+          "2",
+          "5",
+          "3",
+          "1",
+          "4",
+          "3",
+          "5",
+          "2"
+        ]
+      },
+      {
+        name: "Stewie",
+        photo: "http://www.pngmart.com/files/3/Family-Guy-PNG-Transparent-Picture.png",
+        scores: [
+          "5",
+          "1",
+          "3",
+          "4",
+          "2",
+          "4",
+          "3",
+          "5",
+          "3",
+          "5"
+        ]
+      }
+  
+  ];
+  
+  module.exports = friends;  
